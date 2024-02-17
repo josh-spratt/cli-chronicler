1b34d9c624c7cf49217ea11bc016597a1eb2cc93
Josh Spratt <jspratt02@icloud.com>
2024-02-17 15:35:34 -0600

    Updates for pypi (#9)
    * finish v0 for pypi
    
    * update changelog

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    desc = f.read()
+
+setup(
+    name="cli_chronicler",
+    version="0.4.0",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["punch = cli_chronicler:main"]},
+    long_description=desc,
+    long_description_content_type="text/markdown"
+)
